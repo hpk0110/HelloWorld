473d9f4d61e07473c6657d9566f885d0b511eec6
WIN-3A3SVFMMI2R\Administrator <https://github.com/>
2023-06-06 00:01:46 +0800

    11

---
+++ src/TestDowhile.java
@@ -0,0 +1,9 @@
+public class TestDowhile {
+	public static void main (String[] args) {
+		int x =10;
+    	do {
+    		System.out.println("x的值是"+x);
+    		x++;
+    		System.out.println("\n");    	}while(x<20);
+    }
+}
